f68bae5226070afb3b1fa3a09936fb07fc8e92af
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-15 19:40:20 +0500

    Day 3: (Control Structures)

---
+++ Day_03_Control_Structures/app.js
@@ -0,0 +1,11 @@
+// Day 3: Control Structures
+// Activity 1: If-Else Statements
+// Task 1
+let userInp = +prompt("Enter Number");
+if (userInp > 0) {
+  console.log(`The Number ${userInp} is Positive`);
+} else if (userInp < 0) {
+  console.log(`The Number ${userInp} is Negative`);
+} else {
+  console.log(`The Number ${userInp} is Zero`);
+}
