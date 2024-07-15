e1abb6f2cf4121d50873f3b4046161c6650cf76f
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-15 20:08:25 +0500

    Nested If-Else

--- Day_03_Control_Structures/app.js
+++ Day_03_Control_Structures/app.js
@@ -16,3 +16,58 @@ if (userAge >= 18) {
 } else {
   console.log(`You are not Eligible for Vote`);
 }
+
+// Activity 2: Nested If-Else Statements
+// Task 3
+function findLargest(a, b, c) {
+  if (a >= b) {
+    if (a >= c) {
+      console.log(`The largest number is ${a}`);
+    } else {
+      console.log(`The largest number is ${c}`);
+    }
+  } else {
+    if (b >= c) {
+      console.log(`The largest number is ${b}`);
+    } else {
+      console.log(`The largest number is ${c}`);
+    }
+  }
+}
+// Test the function
+// findLargest(3, 5, 2); // Output: The largest number is 5
+// findLargest(12, 2, 8); // Output: The largest number is 12
+
+// Activity 3: Switch Case
+// Task 4
+function checkDay(day) {
+  switch (day) {
+    case 1:
+      console.log("Sunday");
+      break;
+    case 2:
+      console.log("Monday");
+      break;
+    case 3:
+      console.log("Tuesday");
+      break;
+    case 4:
+      console.log("Wednesday");
+      break;
+    case 5:
+      console.log("Thursday");
+      break;
+    case 6:
+      console.log("Friday");
+      break;
+    case 7:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Invalid day");
+  }
+}
+// checkDay(1);
+// checkDay(5);
+
+// Task 5
