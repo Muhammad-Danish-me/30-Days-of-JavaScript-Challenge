afa5a037d4a0eec0c06ad277c19b3f0cb2b52fd8
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-16 23:34:47 +0500

    Day 4: (Loops)

---
+++ Day_04_Loops/app.js
@@ -0,0 +1,12 @@
+// Days 4: Loops
+// Activity 1: For Loop
+// Task 1:
+for (var i = 1; i <= 10; i++) {
+  console.log(i);
+}
+// Task 2:
+for (var i = 2; i <= 20; i++) {
+  console.log(`2 X ${i / 2} = ${i++}`);
+}
+
+// Activity 2: While Loop 
