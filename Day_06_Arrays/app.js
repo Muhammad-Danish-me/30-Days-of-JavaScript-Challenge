32bed9ed7c1a579baf09bd6577e1006a0daeaeee
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-07-19 00:16:45 +0500

    Day 6 (Arrays)

---
+++ Day_06_Arrays/app.js
@@ -0,0 +1,11 @@
+// Arrays
+// Activity 1: Array Creation and Access
+// Task 1
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// Task 2
+let numFisrstEle = numbers[0];
+console.log(numFisrstEle);
+let numLastEle = numbers[4];
+console.log(numLastEle);
